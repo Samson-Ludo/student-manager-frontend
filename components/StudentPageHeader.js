57ed9d2b70acaf4ffb0faf1b3a6917a702787133
Samson Okaludo <samson@divercity.io>
2022-01-17 22:35:16 +0100

    integrated Students courses page with api and components

--- components/StudentPageHeader.js
+++ components/StudentPageHeader.js
@@ -3,7 +3,7 @@ function StudentPageHeader({ studentName, }) {
     <div className="py-4 px-auto">
       <ul className="flex">
         <li className="mr-6">
-          <h1 className="text-blue-700 text-2xl font-bold mb-2">{`${studentName}`}</h1>
+          <h1 className="text-blue-700 text-2xl font-bold mb-2">Courses being taken by {studentName}</h1>
         </li>
         <li className="mr-6">
           
