07ae79943dfb3e2054cc39644acdb7fae465ef33
Samson Okaludo <samson@divercity.io>
2022-01-17 19:22:02 +0100

    create AppBar component

--- components/index.js
+++ components/index.js
@@ -3,5 +3,6 @@ import FormModal from "./FormModal"
 import AddStudentForm from "./AddStudentForm"
 import AddCourseForm from "./AddCourseForm"
 import SingleCard from "./SingleCard"
+import AppBar from "./AppBar"
 
-export {Loader, FormModal, AddStudentForm, AddCourseForm , SingleCard}
+export {Loader, FormModal, AddStudentForm, AddCourseForm , SingleCard, AppBar}
